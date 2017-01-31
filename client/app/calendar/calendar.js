d659016233dce11927203f1527c40f4d1c24dd71
Alex Autem <autem.alex@gmail.com>
2017-01-31 22:19:48 +0100

    Set default view to 'daily' for calendars

--- client/app/calendar/calendar.js
+++ client/app/calendar/calendar.js
@@ -7,9 +7,9 @@ angular.module('app.calendar', ['ngSanitize'])
       return $sce.trustAsResourceUrl(src);
     };
 
-    $scope.username = "autem.alex";
+    $scope.username = "communicativenewts";
     $scope.timezone = "America/New_York";
 
-    $scope.calSource = "https://calendar.google.com/calendar/embed?src=" + $scope.username + "%40gmail.com&ctz=" + $scope.timezone;
+    $scope.calSource = "https://calendar.google.com/calendar/embed?showPrint=0&mode=AGENDA&src=" + $scope.username + "%40gmail.com&ctz=" + $scope.timezone;
 
   });
