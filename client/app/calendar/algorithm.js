8cb19b300374cd851e183faa42525a1dd9623540
Elizabeth Akerman <ekakerman@gmail.com>
2017-02-02 07:34:03 -0800

    add method for making a "day" array for planning the schedule

--- client/app/calendar/algorithm.js
+++ client/app/calendar/algorithm.js
@@ -2,7 +2,20 @@ angular.module('app.factory', [])
 
 .factory('Algorithm', function () {
 
+  var makeDay = function(start, end) {
+    var day = [];
+    for (var i = userData.dayStart; i <= userData.dayEnd; i += 0.5) {
+      day[i * 2] = {
+        timeBlock: i
+      };
+    }
+    return day;
+  };
+
+
+
   return {
+    makeDay: makeDay;
 
   };
 })
