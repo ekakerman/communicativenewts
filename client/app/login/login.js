7e7d771179c8bbddc710bc48eb73be3690f6069a
Bruce <brucegraham11@gmail.com>
2017-02-04 16:04:01 -0800

    Refactored the Sign in/Sign out buttons and tweeked some minor formating in login.js and login.html.

--- client/app/login/login.js
+++ client/app/login/login.js
@@ -1,6 +1,4 @@
-console.log('made it to login.js');
 angular.module('app.login', [])
   .controller('loginCtrl', function($scope) {
-    $scope.value = 'Ready to get started?  Sign in with google up here!';
-
-  })
+    $scope.value = 'Sign in with your Google Calendar in the upper right corner.';
+  });
