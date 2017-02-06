6810fd5c4dff40bdbacdb339c48205f844616277
Elizabeth Akerman <ekakerman@gmail.com>
2017-02-06 08:55:13 -0800

    remove unused routes

--- server/config/routes.js
+++ server/config/routes.js
@@ -2,12 +2,5 @@ var taskController = require('../tasks/taskController.js');
 var userController = require('../users/userController.js');
 
 module.exports = function (app, express) {
-  app.post('api/users/signin', userController.signin);
   app.post('api/users/signup', userController.signup);
-  app.post('api/users/update', userController.update);
-
-  app.get('api/tasks', taskController.newTask);
-  app.post('api/tasks', taskController.changeTask);
-  app.post('api/tasks/:delete', taskController.deleteTask);
-
 }
